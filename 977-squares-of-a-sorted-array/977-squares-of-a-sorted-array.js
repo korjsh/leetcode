393ea06be7750806da62789769a2c16bb4a5a965
korjsh <76986399+korjsh@users.noreply.github.com>
2022-08-29 23:41:32 +0900

    Time: 206 ms (16.17%), Space: 48.5 MB (78.37%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    const arr = nums.map(item => item*item)
+    return arr.sort((a,b) => {
+        return a-b
+    })
+    
+};
