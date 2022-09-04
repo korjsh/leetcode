37af87a13687bb619c4b60d4c1a15e667456fde2
korjsh <76986399+korjsh@users.noreply.github.com>
2022-09-04 21:57:51 +0900

    Time: 114 ms (20.61%), Space: 42.9 MB (62.06%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -3,12 +3,21 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function(numbers, target) {
+//     const map = {}
+//     for (let i=0; i<numbers.length; i++) {
+//         if (map[target - numbers[i]] !== undefined) {
+//             return [map[target - numbers[i]]+1, i+1]
+//         }
+//             map[numbers[i]] = i
+//     }
+// };
 var twoSum = function(numbers, target) {
-    const map = {}
-    for (let i=0; i<numbers.length; i++) {
-        if (map[target - numbers[i]] !== undefined) {
-            return [map[target - numbers[i]]+1, i+1]
-        }
-            map[numbers[i]] = i
+    let st = 0
+    let en = numbers.length-1
+    while(numbers[st] + numbers[en] !== target) {
+        if(numbers[st] + numbers[en] > target) en--
+        else st++
     }
+    return [st+1, en+1]
 };
