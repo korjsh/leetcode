eb10cd0e989404636f299509328e78b45d23fc76
korjsh <76986399+korjsh@users.noreply.github.com>
2022-09-04 18:50:30 +0900

    Time: 160 ms (50.86%), Space: 49.1 MB (22.33%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -2,14 +2,29 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    const arr = nums.map(item => item*item)
-    return arr.sort((a,b) => {
-        return a-b
-    })
-    
-};
 // var sortedSquares = function(nums) {
-    
+//     const arr = nums.map(item => item*item)
+//     return arr.sort((a,b) => {
+//         return a-b
+//     })
     
 // };
+var sortedSquares = function(nums) {
+    const len = nums.length
+    let st = 1
+    let en = len
+    let idx = len
+    let arr = new Array(len)
+    
+    while(idx >= 1) {
+        if(nums[en-1]**2 > nums[st-1]**2) {
+            arr[idx-1] = nums[en-1]**2
+            en--
+        } else {
+            arr[idx-1] = nums[st-1]**2
+            st++
+        }
+        idx--
+    }
+    return arr
+};
